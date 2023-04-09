6e4069abee46d269b447fb10b61f91a0ef691426
Siddarth R <sroyapa@ncsu.edu>
2023-04-09 18:04:09 -0400

    test

--- api/routes.go
+++ api/routes.go
@@ -1,11 +1,22 @@
 package api
 
 import (
+	"net/url"
+
 	"github.com/SiddarthR56/Auto-scaling-framework-for-containers/handlers"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func SetApiRoutes(e *echo.Echo) {
+
+	url1, _ := url.Parse("http://localhost:8081")
+
+	targets := []*middleware.ProxyTarget{
+		{
+			URL: url1,
+		},
+	}
 
 	api := e.Group("/api")
 
@@ -14,5 +25,8 @@ func SetApiRoutes(e *echo.Echo) {
 	api.POST("/deletenode", handlers.DeleteNode)
 
 	api.POST("/frequest", handlers.ProxyRequest)
+
+	g := e.Group("/blog")
+	g.Use(middleware.Proxy(middleware.NewRoundRobinBalancer(targets)))
 
 }
--- handlers/request.go
+++ handlers/request.go
@@ -1,11 +1,13 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/SiddarthR56/Auto-scaling-framework-for-containers/common"
 	"github.com/labstack/echo"
 )
 
 func ProxyRequest(c echo.Context) error {
 	common.Lb(c.Response().Writer, c.Request())
-	//return c.JSON(http.StatusOK, "Message: Ok")
+	return c.JSON(http.StatusOK, "Message: Ok")
 }
