bc8e6429ef7797172e2f09e4a40558939d3b363a
Jyothi Sumer <59790162+J-Sumer@users.noreply.github.com>
2023-04-20 16:06:32 -0400

    Adding debugger

--- handlers/request.go
+++ handlers/request.go
@@ -2,12 +2,13 @@ package handlers
 
 import (
 	"net/http"
-
+	"fmt"
 	"github.com/SiddarthR56/Auto-scaling-framework-for-containers/common"
 	"github.com/labstack/echo"
 )
 
 func ProxyRequest(c echo.Context) error {
+	fmt.Println("Request came")
 	common.Lb(c.Response().Writer, c.Request())
 	return c.JSON(http.StatusOK, "Message: Ok")
 }
