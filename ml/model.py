f9bdc91f9f0753644c6bf36386ea413c7baa220b
Siddarth R <sroyapa@ncsu.edu>
2023-04-27 00:42:25 -0400

    ML changes

--- ml/model.py
+++ ml/model.py
@@ -1,43 +1,44 @@
+import os
 import pickle
+import numpy as np
 from keras.models import Sequential
-from keras.layers import Bidirectional, BatchNormalization
-from keras.layers import Dropout, InputLayer, LSTM, CuDNNLSTM
+from keras.layers import Bidirectional
+from keras.layers import Dropout, LSTM, RepeatVector
 from keras.layers import Dense, BatchNormalization
-from tensorflow.keras.optimizers import SGD, Adam, RMSprop
+from tensorflow.keras.optimizers import Adam
+
+Window = 5
 
 class BILSTM:
 
     def __init__(self) -> None:
         self.model = self.build()
+        if os.path.exists('ml/model.pkl'):
+            self.load_model('ml/model.pkl')
 
 
     def build(self):
         model = Sequential()
-        model.add(Bidirectional(LSTM(128, return_sequences=True, input_shape=(WIND_SIZE,6)))) 
-        model.add(BatchNormalization())
-        model.add(Dropout(0.3)) 
-        model.add((LSTM(128, return_sequences=True))) 
-        model.add(BatchNormalization())
-        model.add(Dropout(0.5))
-        model.add(Dense(15, activation="relu"))
-        model.add(BatchNormalization())
-        model.add(Dropout(0.5))  
-        model.add(Dense(4, activation="softmax"))
+        model.add(Bidirectional(LSTM(units=64, input_shape=( Window, 3))))
+        model.add(RepeatVector(n=Window))
+        model.add(Bidirectional(LSTM(units=64)))
+        model.add(Dense(15, activation="elu"))
+        model.add(Dropout(0.2))
+        model.add(Dense(units=1, activation="elu"))
+
+        adamOpt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, decay=0.0, amsgrad=False)
+        model.compile(loss='mean_squared_error', optimizer=adamOpt, metrics=['mae'])
 
         return model
     
     def load_model(self, path):
         self.model = pickle.load(open(path, 'rb'))
+
+    def predict(self, X_test):
+        return self.model.predict(np.array([X_test]))[0]
     
     def save_model(self, path):
         pickle.dump(self.model, open(path, 'wb'))
-
-    def compile(self, model):
-        model.compile(loss='categorical_crossentropy',
-                    optimizer=Adam(lr=0.0001, decay=1e-6),
-                    metrics=['accuracy'])
-        return model
-    
 
     def train(self, model, X_train, y_train, X_test, y_test):
         model.fit(X_train, y_train,
@@ -47,6 +48,5 @@ class BILSTM:
         return model
     
 
-    def predict(self, model, X_test):
-        return model.predict(X_test)
+    
     
